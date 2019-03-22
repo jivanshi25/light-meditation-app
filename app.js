80f17c592049c652e29020bccffb35741811a269
Viacheslav Shvorak <viacheslav.shvorak@gmail.com>
2019-03-22 20:09:44 +0200

    add some logic in app script

---
+++ app.js
@@ -0,0 +1,70 @@
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+  //Time Display
+  const timeDisplay = document.querySelector(".time-display");
+  //Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  //Duration
+  const timeSelect = document.querySelectorAll(".time-select button");
+  let fakeDuration = 600;
+
+  outline.style.strokeDashoffset = outlineLength;
+  outline.style.strokeDasharray = outlineLength;
+  timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+    fakeDuration % 60)}`;
+
+  //Pick diff sounds  
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+  //Play Sound
+  play.addEventListener("click", function() {
+    checkPlaying(song);
+  });
+  
+  timeSelect.forEach(option => {
+    option.addEventListener("click", function() {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60)}`;
+    });
+  });
+  //Stop and play sounds function
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
+  //Animate time
+  song.ontimeupdate = function() {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+    //Animate text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+    //Animate circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
+  };
